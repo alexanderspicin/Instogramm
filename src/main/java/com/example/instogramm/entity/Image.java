b86d35bdadaa55b1601f8c47d40e90e763c8eca4
Alexander Spicin <aspicin@fromtech.ru>
2021-10-30 14:36:49 +0300

    Create Entity and Repo

---
+++ src/main/java/com/example/instogramm/entity/Image.java
@@ -0,0 +1,29 @@
+package com.example.instogramm.entity;
+
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Data
+@Entity
+public class Image {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] imageBytes;
+
+    @JsonIgnore
+    private Long userId;
+
+    @JsonIgnore
+    private Long postId;
+}
